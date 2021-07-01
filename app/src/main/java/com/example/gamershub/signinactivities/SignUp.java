c63bf566c9322602851c2cf2a57cae4030a96d56
Basu Singh <basu.singh05@gmail.com>
2021-07-02 00:43:46 +0530

    SignIn, signUp, userInfo layouts

---
+++ app/src/main/java/com/example/gamershub/signinactivities/SignUp.java
@@ -0,0 +1,16 @@
+package com.example.gamershub.signinactivities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.gamershub.R;
+
+public class SignUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
---
+++ app/src/main/java/com/example/gamershub/signinactivities/UserInfo.java
@@ -0,0 +1,16 @@
+package com.example.gamershub.signinactivities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.gamershub.R;
+
+public class UserInfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_info);
+    }
+}
